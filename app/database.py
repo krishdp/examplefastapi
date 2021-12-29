0739702fd0a3a37c3f942e97200f87adeec50220
he11o-wo1ld <roykrish9899@gmail.com>
2021-12-29 12:48:19 +0530

    adding some extra files

--- app/database.py
+++ app/database.py
@@ -8,6 +8,8 @@ from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
+
+
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password1234@localhost:5432/fastapi2"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
